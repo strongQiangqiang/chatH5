995ffb8e950814bc3e96c69aaec909d5fb57073d
liqiang <liqiang01@jcgroup.com.cn>
2018-06-25 16:02:12 +0800

    添加简单高阶组件

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -1,38 +1,83 @@
 // 登录页面
 import React, { Component } from 'react'
-import Logo from '../../component/logo/logo'
 import { List, InputItem, WingBlank, WhiteSpace, Button } from 'antd-mobile'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import { login } from '../../redux/user.redux'
- 
+import Logo from 'Component/logo/logo'
+import imoocFrom from 'Component/imooc-form/imooc-form'
+
+// 高阶组件的理解，函数当参数和函数当返回值
+/*
+function hello() {
+  console.log('hello')
+}
+function wrapperHello(fn) {
+  return function() {
+    console.log('before say hello')
+    fn()
+    console.log('after say hello')
+  }
+}
+hello = wrapperHello(hello)
+hello()
+*/
+
+// 可以当做引的外部组件
+function WrapperHello(Comp) {
+  class WrapComp extends React.Component{
+    render() {
+      return (
+        <div>
+          <p>这是HOC高阶组件特有的元素</p>
+          <Comp { ...this.props }></Comp>
+        </div>
+      )
+    }
+  }
+  /*
+    // 反向继承，可以做渲染劫持
+    class WrapComp extends Comp{
+      componentDidMount() {
+        console.log('高阶组件新增的生命周期，加载完成')
+      }
+      render() {
+        return <Comp />
+      }
+    }
+  */
+  return WrapComp
+}
+// 其实所谓的高阶组件就是给一个组件,返回另外一个组件,另外一个组件吧原来的组件包裹一层，这样我们就在原有的基础上可以添加或者增强一些东西
+// 主要有两种功能的高阶组件，使用高阶组件具体的作用就是代码复用，逻辑抽象
+// 第一种叫属性代理:我们可以个上面Comp加任意的属性，也可以在Comp上面或者下面添加任意的元素
+// 第二种叫反向继承
+@WrapperHello
+class Hello extends React.Component{
+  render() {
+    return <h2>hello haha I love</h2>
+  }
+}
+// Hello = WrapperHello(Hello)
+
 @connect(
   state => state.user,
   {login}
 )
-
+// 这里我们用高阶组件包裹下
+@imoocFrom
 class Login extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      user: '',
-      pwd: ''
-    }
     this.register = this.register.bind(this)
     this.handleLogin = this.handleLogin.bind(this)
-    this.is = this.is.bind(this)
   }
   register() {
     this.props.history.push('/register')
   }
-  handleChange(key, val) {
-    this.setState({
-      [key]:val
-    })
-  }
   // 点击登录
   handleLogin() {
-    this.props.login(this.state)
+    this.props.login(this.props.state)
   }
   is(str) {
   }
@@ -46,8 +91,8 @@ class Login extends Component {
         <WingBlank>
           <List>
             { msg ? <p className='error-msg'>{msg}</p> : '' }
-            <InputItem onChange={v => this.handleChange('user', v)}>用户</InputItem>
-            <InputItem onChange={v => this.handleChange('pwd', v)}>密码</InputItem>
+            <InputItem onChange={v => this.props.handleChange('user', v)}>用户</InputItem>
+            <InputItem onChange={v => this.props.handleChange('pwd', v)}>密码</InputItem>
           </List>
           <WhiteSpace />
           <Button onClick={this.handleLogin} type='primary'>登录</Button>
--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,67 +1,67 @@
 import React, { Component } from 'react'
-import Logo from '../../component/logo/logo'
 import { List, InputItem, Radio, WhiteSpace, Button } from 'antd-mobile'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import { register } from '../../redux/user.redux'
+import Logo from 'Component/logo/logo'
+import imoocFrom from 'Component/imooc-form/imooc-form'
 
 const RadioItem = Radio.RadioItem
 @connect(
   state => state.user,
   { register }
 )
-
+@imoocFrom
 class Register extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      user: '', // 账号
-      pwd: '', // 密码
-      repeatpwd: '', // 确认密码
-      type: 'genius' // Boss
-    }
+    // this.state = {
+    //   user: '', // 账号
+    //   pwd: '', // 密码
+    //   repeatpwd: '', // 确认密码
+    //   type: 'genius' // Boss
+    // }
     this.handRegister = this.handRegister.bind(this)
   }
-  handleChange(key, val) {
-    this.setState({
-      [key]:val
-    })
+  componentDidMount() {
+    // 这里因为有默认值，用高阶组件的时候要在didmount里面设置下
+    this.props.handleChange('type', 'genius')
   }
   handRegister() {
-    this.props.register(this.state)
-    console.log(this.state)
+    this.props.register(this.props.state)
+    console.log(this.props.state)
   }
   render() {
-    const { type } = this.state
+    const { redirectTo, msg, state } = this.props
     return (
       <div>
-        {this.props.redirectTo ? <Redirect to={this.props.redirectTo} /> : null}
+        {this.props.redirectTo ? <Redirect to={redirectTo} /> : null}
         <Logo></Logo>
         <List>
-          { this.props.msg ? <p className='error-msg'>{this.props.msg}</p> : '' }
+          { msg ? <p className='error-msg'>{msg}</p> : '' }
           <InputItem
-            onChange={v => this.handleChange('user', v)}
+            onChange={v => this.props.handleChange('user', v)}
           >用户</InputItem>
           <WhiteSpace />
           <InputItem
             type = 'password'
-            onChange={v => this.handleChange('pwd', v)}
+            onChange={v => this.props.handleChange('pwd', v)}
           >密码</InputItem>
           <WhiteSpace />
           <InputItem
             type = 'password'
-            onChange={v => this.handleChange('repeatpwd', v)}
+            onChange={v => this.props.handleChange('repeatpwd', v)}
           >确认密码</InputItem>
           <WhiteSpace />
           <RadioItem
-            checked={ type === 'genius' }
-            onChange={() => this.handleChange('type', 'genius')}
+            checked={ state.type === 'genius' }
+            onChange={() => this.props.handleChange('type', 'genius')}
           >
             牛人
           </RadioItem>
           <RadioItem
-            checked={ type === 'boss' }
-            onChange={() => this.handleChange('type', 'boss')}
+            checked={ state.type === 'boss' }
+            onChange={() => this.props.handleChange('type', 'boss')}
           >
             Boss
           </RadioItem>
